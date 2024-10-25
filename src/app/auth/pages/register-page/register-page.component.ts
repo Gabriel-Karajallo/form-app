13eaaf84147cba768b2b84281d6d935e81344e31
Gabriel-Karajallo <c.gabrielkarajallo@gmail.com>
2024-10-25 13:08:31 +0200

    form-app

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html'
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup;
 
+
+  constructor (private fb: FormBuilder){
+    this.myForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required], []],
+      username: ['', [Validators.required], []],
+      password: ['', [Validators.required, Validators.minLength(6)], []],
+      password2: ['', [Validators.required], []],
+    });
+  }
+
+
+  isValidField( field: string){
+    //TODO: OBTENER VALIDACION DESDE UN SERVICIO
+  }
+
+  onSubmit(){
+    this.myForm.markAllAsTouched();
+  }
 }
--- src/app/reacive/pages/switches-page/switches-page.component.ts
+++ src/app/reacive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,41 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
 export class SwitchesPageComponent {
+  public myForm: FormGroup;
+
+
+  constructor(private fb: FormBuilder){
+    this.myForm = this.fb.group({
+      gender: ['', Validators.required],
+      wantNotification: [true, Validators.required],
+      termsAndConditions:[false, Validators.requiredTrue],
+    })
+  }
+
+
+
+  onSave(){
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return
+    }
+  }
+
+
+  // --------------------------------------VALIDACION--------------------------------------------------
+  isValidField( field: string ): boolean | null{
+    return this.myForm.controls[field].errors
+        && this.myForm.controls[field].touched;
+  }
+// ----------------------------------------------------------------------------------------------------
+
+
+
+
 
 }
